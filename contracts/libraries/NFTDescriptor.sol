24cc0c7441cee35f4834105837acb55ffd130bb6
zandent <simon.zhao@mail.utoronto.ca>
2023-04-12 15:58:39 -0400

    change to vSwap

--- contracts/libraries/NFTDescriptor.sol
+++ contracts/libraries/NFTDescriptor.sol
@@ -112,7 +112,7 @@ library NFTDescriptor {
         return
             string(
                 abi.encodePacked(
-                    'This NFT represents a liquidity position in a Uniswap V3 ',
+                    'This NFT represents a liquidity position in a vSwap V3 ',
                     quoteTokenSymbol,
                     '-',
                     baseTokenSymbol,
@@ -160,7 +160,7 @@ library NFTDescriptor {
         return
             string(
                 abi.encodePacked(
-                    'Uniswap - ',
+                    'vSwap - ',
                     feeTier,
                     ' - ',
                     escapeQuotes(params.quoteTokenSymbol),
